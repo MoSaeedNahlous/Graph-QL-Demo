857d7fcb17bd04ab9455b5352628d1b005888f17
MoSaeedNahlous <jay.haji041@gmail.com>
2021-03-19 11:57:01 +0200

    Models Added

--- server/app.js
+++ server/app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 
 const app = express();
 
+
+mongoose.connect('mongodb://localhost/gql-db');
+mongoose.connection.once('open', () => {
+    console.log("connected to DataBase!")
+})
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
     schema,graphiql:true
