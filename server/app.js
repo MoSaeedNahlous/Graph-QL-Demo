046f036a3d8bc55b92ddb972edab303b37c1519a
MoSaeedNahlous <jay.haji041@gmail.com>
2021-04-23 20:35:48 +0300

    cloud database

--- server/app.js
+++ server/app.js
@@ -17,16 +17,22 @@ const app = express();
 //allow cros origin requests
 app.use(cors());
 
-mongoose.connect('mongodb://localhost/gql-db');
+// mongoose.connect('mongodb://localhost/gql-db');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://youssefkafa:123kafa123@libapp.u0dkt.mongodb.net/libApp-database?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true, dbName: 'libApp-database' }).then(() => {
+        console.log("Connected to Mongodb on Cloud")
+    }).catch(err => {
+        console.log(err)
+    })
 
 
-mongoose.connection.once('open', () => {
-    console.log("connected to DataBase!")
-})
+// mongoose.connection.once('open', () => {
+//     console.log("connected to DataBase!")
+// })
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
-    schema,graphiql:true
+    schema, graphiql: true
 }));
 
 
