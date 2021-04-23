2dc51b2642b4210c316e9985f6c41477a277d507
MoSaeedNahlous <jay.haji041@gmail.com>
2021-04-23 20:15:44 +0300

    NOTHING REALLY MATTER

--- client/src/components/AddNewAuthor.js
+++ client/src/components/AddNewAuthor.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import { addAuthorMutation,getAuthorsQuery } from '../queries/queries.js'
 import { graphql } from 'react-apollo'
 
